06d798b29934d9c56e59f996a2bbde3ab79beff4
Adam Kowalewski <adam.kowalewski@student.put.poznan.pl>
2023-06-09 21:31:00 +0200

    Init project commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { StudentService } from './student.service';
+import { HttpClientModule } from '@angular/common/http';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    StudentDetailComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [StudentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
